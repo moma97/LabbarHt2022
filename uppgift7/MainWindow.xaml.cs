8d6d8b5a4cad51c906f9f60420aa632132dcc3b4
Monira Mah <monira97@hotmail.com>
2022-11-04 16:34:30 +0100

    Löst uppgift 7

---
+++ uppgift7/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace uppgift7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+           int tal1 = int.Parse(txttal1.Text);
+            int tal2 = int.Parse(txttal2.Text);
+            
+           
+            double division = tal1 / tal2;
+
+            txtheltal.Text = division.ToString();
+
+            int total;
+
+            total = tal1 % tal2;              
+
+            txtrest.Text = total.ToString();
+
+        }
+    }
+}
