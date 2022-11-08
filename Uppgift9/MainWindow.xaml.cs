52d8f2e27d4876fe03fb879134360de801b53ad2
Monira Mah <monira97@hotmail.com>
2022-11-08 01:26:01 +0100

    uppgift9

--- Uppgift9/MainWindow.xaml.cs
+++ Uppgift9/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace Uppgift9
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        double Galleoner = 1;
+        double Sikel = 17;
+        double Knuting = 493;
+        double SvenskaKronor = 62.02;
+        double exchangerategall = 0.0161;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double Galleoner = double.Parse(txtGalleoner.Text);
+
+            lblsumma.Content = Galleoner / exchangerategall;
+
+            
+
+                 //double Sikel = double.Parse(txtSiklar.Text.ToString);
+           // double Knuting = double.Parse(txtKnutar.Text);
         }
     }
 }
