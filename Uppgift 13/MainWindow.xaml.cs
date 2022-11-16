bed9f454e8f8c7b17af02bc90b297da9e9040b91
Monira Mah <monira97@hotmail.com>
2022-11-16 15:50:42 +0100

    Uppgift 13 klar

--- Uppgift 13/MainWindow.xaml.cs
+++ Uppgift 13/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace Uppgift_13
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        // datatyp, array, skriver i denna del så jag slipper upprepa mig 
+        string[] fruits = { "äpple", "päron", "banan", "ananas", "melon", "kiwi" };
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,16 +31,41 @@ namespace Uppgift_13
 
         private void btnfirst_Click(object sender, RoutedEventArgs e)
         {
-            string[] fruits = new string[4];
+            MessageBox.Show("Första frukten i listan är: " + fruits[0]);
 
-            //ange antal fack
+        }
 
-            fruits[0] = "Äpple";
-            fruits[1] = "Päron";
+        private void btnmiddle_Click(object sender, RoutedEventArgs e)
+        {
+
+            //skapa två olika if satser, ena där antalet frukter är jämnt delbart med två och en där den inte är det. Vid jämnt delbart med två, visa två mitten värden, om inte delbart, visa endast 1
+
+
+            // om fruits[fruit.Lenght -1] är sista rutan så borde jag kunna använda samma koncept att få fram mitten värden oavsett hur många som finns. 
+
+            int middlefruit = fruits.Length;
+
+            string middfruit = fruits[middlefruit / 2];
+
+            string middfruit2 = fruits[middlefruit / 2 - 1];
+
+            if (middlefruit % 2 == 0)
+                MessageBox.Show("Frukterna i mitten är: " + middfruit + " och " + middfruit2);
+
+            else
+                MessageBox.Show("frukten i mitten är: " + middfruit);
 
             
 
+        }
 
+        private void btnlast_Click(object sender, RoutedEventArgs e)
+        {      
+            //skapa en variabel för den sista frukten i arrayen
+
+            string lastFruit = fruits[fruits.Length - 1];
+
+            MessageBox.Show("Sista frukten i listan är: " + lastFruit);
         }
     }
 }
